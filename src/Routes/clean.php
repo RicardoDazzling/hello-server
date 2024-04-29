7c5994f93e868c1c5d39a7e78adb7fb233bbd5d6
Ricardo Állan Costa <118565541+RicardoDazzling@users.noreply.github.com>
2024-04-28 21:58:46 -0300

    Final edit

--- src/Routes/clean.php
+++ src/Routes/clean.php
@@ -2,16 +2,20 @@
 
 use DazzRick\HelloServer\Exceptions\BadRequestException;
 use DazzRick\HelloServer\Services\FileService;
+use DazzRick\HelloServer\Services\GFileService;
+use DazzRick\HelloServer\Services\GMessageService;
 use DazzRick\HelloServer\Services\LostService;
 use DazzRick\HelloServer\Services\MessageService;
 
 function getResponse(): string
 {
-    $table = $_REQUEST['table'] ?? null;
+    $table = empty($_REQUEST['subresource']) ? null : $_REQUEST['subresource'];
     if(is_null($table)) throw new BadRequestException('Argument required: "table"');
     $service = match ($table) {
         'message' => new MessageService(),
         'file' => new FileService(),
+        'gmessage' => new GMessageService(),
+        'gfile' => new GFileService(),
         'lost' => new LostService(),
         default => throw new BadRequestException('Unknown table')
     };
--- src/Services/GBaseService.php
+++ src/Services/GBaseService.php
@@ -9,6 +9,7 @@ use DazzRick\HelloServer\Entity\Entitable;
 use DazzRick\HelloServer\Entity\GFile;
 use DazzRick\HelloServer\Entity\GMessage;
 use DazzRick\HelloServer\Exceptions\BadRequestException;
+use DazzRick\HelloServer\Exceptions\NotFoundException;
 use DazzRick\HelloServer\Exceptions\UnAuthorizedException;
 use DazzRick\HelloServer\Exceptions\ValidationException;
 use DazzRick\HelloServer\Validation\GBaseValidation;
@@ -67,6 +68,12 @@ class GBaseService implements Serviceable
             ->setUuid(Uuid::uuid4()->toString())
             ->setSent(intdiv(time(), 60));
 
+        $participant = ParticipantDAL::get($GLOBALS['jwt']->getUuid(), $entity->getToUuid());
+        if ($participant->isEmpty())
+            throw new UnAuthorizedException('You don\'t is a participant.');
+        if (!$participant->isActive())
+            throw new UnAuthorizedException('You are not a Active Member.');
+
         $entity = static::new_dal_instance()->create($entity);
         if ($entity->isEmpty()) Http::setHeadersByCode(StatusCode::INTERNAL_SERVER_ERROR);
         return $entity;
@@ -116,6 +123,13 @@ class GBaseService implements Serviceable
     {
         if(is_null($uuid)) throw new ValidationException("UUID is required.");
         if (!v::uuid()->validate($uuid)) throw new ValidationException("Invalid message UUID");
+        $dal = static::new_dal_instance();
+        $old_entity = $dal::get($uuid);
+        if($old_entity->isEmpty())
+            throw new NotFoundException('Message not found');
+        $classification = self::userClassification($old_entity);
+        if ($classification !== self::SENDER)
+            throw new UnAuthorizedException('Only the sender user can remove the message.');
         $entity = static::new_dal_instance()->remove($uuid);
         if ($entity->isEmpty()) throw new ValidationException("Unknown entity.");
         return $entity;
